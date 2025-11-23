063ba423842c39fd699e7bfd50229db013bbd19f
kendrapierre-25 <Kendrapierre@usf.edu>
2025-11-23 18:52:55 -0500

    Add basic error handling for user input

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ study_hours = input("How many hours did you study today?")
 study_hours = float(study_hours)
 weekly_hours = study_hours * 7
 print(f"You are on track to study {weekly_hours} hours this week.")
+try:
+    hours = float(study_hours)
+except ValueError:
+    print("Please enter a valid number.")
+    exit()
