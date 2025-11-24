ef67194f4b3f8d9f3e17b7c60ce4875c95a5b5f0
kendrapierre-25 <Kendrapierre@usf.edu>
2025-11-23 19:07:26 -0500

    Clean up code, add comments, finalize program

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+#Step 1 : print welcome program
 print("Welcome to my Python Program")
+#Step 2: Ask user for number of study hours
 study_hours = input("How many hours did you study today?")
+#Step 3: convert hours into a float to estimate weekly number
 study_hours = float(study_hours)
 weekly_hours = study_hours * 7
+#Step 4: Display Results
+# use f-string
 print(f"You are on track to study {weekly_hours} hours this week.")
+#Step 5: add simple error handling
+
 try:
     hours = float(study_hours)
 except ValueError:
